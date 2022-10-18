772cf10da324057df3615f95be0d820d4d2b943b
Genevieve31200 <116088291+Genevieve31200@users.noreply.github.com>
2022-10-18 16:49:44 +0200

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ffc.model;
+
+/**
+ *
+ * @author sunweichen
+ */
+public abstract class Course {
+    public String nomCourse;
+    
+    public Course(String nomCourse){
+        this.nomCourse=nomCourse;
+    }
+    
+}
